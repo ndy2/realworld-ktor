69fb57c820a4fef2cfc7904035f74f830e13b5f0
ndy2 <emrdbs12@gmail.com>
2023-03-29 12:36:20 +0900

    test: PasswordTest 보충

--- src/test/kotlin/ndy/domain/user/domain/PasswordTest.kt
+++ src/test/kotlin/ndy/domain/user/domain/PasswordTest.kt
@@ -1,9 +1,15 @@
 package ndy.domain.user.domain
 
 import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 import ndy.exception.RealworldRuntimeException
+import ndy.test.generator.UserArbs.passwordEncoderArb
+import ndy.test.generator.UserArbs.passwordValueArb
+import ndy.test.generator.UserArbs.passwordVerifierArb
 import ndy.test.spec.BaseSpec
 
 class PasswordTest : BaseSpec(body = {
@@ -23,6 +29,31 @@ class PasswordTest : BaseSpec(body = {
             // both should be equal
             passwordWithEncoded shouldBe password
         }
+    }
 
+    test("checkPassword 성공/실패") {
+        checkAll(
+            passwordValueArb,
+            passwordEncoderArb,
+            passwordVerifierArb,
+            Arb.string()
+        ) { passwordValue, encoder, verifier, arbString ->
+            // construct password
+            val password = Password(passwordValue, encoder)
+
+            // check success with password
+            shouldNotThrow<RealworldRuntimeException> { password.checkPassword(passwordValue, verifier) }
+
+            // check failure with arb string
+            shouldThrow<RealworldRuntimeException> { password.checkPassword(arbString, verifier) }
+                .apply { message shouldBe "login failure" }
+        }
+    }
+
+    test("cannot access encodedPassword with null args constructed Password"){
+        val nullPassword = Password(rawPassword = null, passwordEncoder = null)
+
+        shouldThrow<RealworldRuntimeException> { nullPassword.encodedPassword /* invoke PasswordDelegate.getValue */ }
+            .apply { message shouldBe "illegal approach to get encodedPassword" }
     }
 })
