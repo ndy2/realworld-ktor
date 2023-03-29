88a5ed57838a0562618fa98dd3552add287eb658
ndy2 <emrdbs12@gmail.com>
2023-03-29 16:13:15 +0900

    refactor UserRoutesTest

--- src/test/kotlin/ndy/routers/user/UserRoutesTest.kt
+++ src/test/kotlin/ndy/routers/user/UserRoutesTest.kt
@@ -2,12 +2,13 @@ package ndy.routers.user
 
 import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.ktor.client.shouldHaveStatus
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.spec.Spec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.kotest.property.checkAll
 import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import ndy.test.generator.ProfileArbs.usernameValueArb
 import ndy.test.generator.UserArbs.emailValueArb
@@ -17,43 +18,29 @@ import ndy.test.spec.BaseSpec
 import ndy.test.util.integrationTest
 import ndy.test.util.registerUser
 
-class UserRoutesTest : BaseSpec(body = {
+class UserRoutesTest : BaseSpec(RequestArb, body = {
     integrationTest("login") { client ->
-        registerArb<RegistrationRequest>(usernameValueArb, emailValueArb, passwordValueArb)
-
-        checkAll<RegistrationRequest> { registrationRequest ->
-            registerUser(client, registrationRequest)
+        checkAll<RegistrationRequest> { request ->
+            registerUser(client, request)
 
             // login with that user
             val response = client.post("/api/users/login") {
                 contentType(ContentType.Application.Json)
-                setBody(mapOf("user" to LoginRequest(registrationRequest.email, registrationRequest.password)))
+                setBody(mapOf("user" to LoginRequest(request.email, request.password)))
             }
 
             response shouldHaveStatus HttpStatusCode.OK
             assertSoftly(response.body<Map<String, UserResponse>>()["user"]!!) {
                 it.token shouldNotBe null
-                it.email shouldBe registrationRequest.email
-                it.username shouldBe registrationRequest.username
+                it.email shouldBe request.email
+                it.username shouldBe request.username
                 it.bio shouldBe null
                 it.image shouldBe null
             }
         }
     }
 
-    integrationTest("login with no user") { client ->
-        val response = client.post("/api/users/login") {
-            contentType(ContentType.Application.Json)
-            setBody("{\"no\":\"user\"}")
-        }
-
-        response shouldHaveStatus HttpStatusCode.BadRequest
-        response.bodyAsText() shouldBe "" // TODO - https://ktor.io/docs/status-pages.html
-    }
-
     integrationTest("signup") { client ->
-        registerArb<RegistrationRequest>(usernameValueArb, emailValueArb, passwordValueArb)
-
         checkAll<RegistrationRequest> { request ->
             val response = client.post("/api/users") {
                 contentType(ContentType.Application.Json)
@@ -70,13 +57,10 @@ class UserRoutesTest : BaseSpec(body = {
             }
         }
     }
-
-    integrationTest("signup with no user") { client ->
-        val response = client.post("/api/users") {
-            contentType(ContentType.Application.Json)
-            setBody("{\"no\":\"user\"}")
-        }
-
-        response shouldHaveStatus HttpStatusCode.BadRequest
-    }
 })
+
+object RequestArb : BeforeSpecListener {
+    override suspend fun beforeSpec(spec: Spec) {
+        registerArb<RegistrationRequest>(usernameValueArb, emailValueArb, passwordValueArb)
+    }
+}
