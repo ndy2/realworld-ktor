1af2294a9fbdf3b0e7a8b6af91a62f37abc65752
ndy2 <emrdbs12@gmail.com>
2023-03-29 14:24:58 +0900

    test: ProfileTest

---
+++ src/test/kotlin/ndy/domain/profile/domain/ProfileTest.kt
@@ -0,0 +1,22 @@
+package ndy.domain.profile.domain
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+import ndy.test.spec.BaseSpec
+
+class ProfileTest : BaseSpec(body = {
+
+    test("create profile") {
+        checkAll<ULong, Username, Bio, Image> { id, username, bio, image ->
+            val profile = Profile(ProfileId(id), username, bio, image)
+
+            assertSoftly(profile){
+                this.id.value shouldBe id
+                this.username shouldBe username
+                this.bio shouldBe bio
+                this.image shouldBe image
+            }
+        }
+    }
+})
